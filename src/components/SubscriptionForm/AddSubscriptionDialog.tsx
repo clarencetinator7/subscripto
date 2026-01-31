6bdeb94abb72488ed478cf94fa5faa5c38380a27
Clarencetinator <clarencejoseph.dimafelix@gmail.com>
2026-01-31 21:04:56 +0800

    add dialog and button components; implement mobile detection hook

--- src/components/SubscriptionForm/AddSubscriptionDialog.tsx
+++ src/components/SubscriptionForm/AddSubscriptionDialog.tsx
@@ -1,4 +1,12 @@
+import useIsMobile from "@/hooks/useIsMobile";
 import { IoIosAdd } from "react-icons/io";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
 import {
   Drawer,
   DrawerContent,
@@ -6,15 +14,34 @@ import {
   DrawerTrigger,
 } from "../ui/drawer";
 import SubscriptionForm from "./SubscriptionForm";
+import { forwardRef, useState, type ComponentProps } from "react";
 
 const AddSubscriptionDialog = () => {
+  const isMobile = useIsMobile();
+  const [open, setOpen] = useState(false);
+
+  if (!isMobile) {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <AddSubscriptionButton />
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader className="p-5">
+            <DialogTitle>
+              <h2 className="text-2xl font-bold">Add Subscription</h2>
+            </DialogTitle>
+          </DialogHeader>
+          <SubscriptionForm />
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
   return (
-    <Drawer>
+    <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
-        <button className="btn flex items-center justify-center p-3 w-full font-semibold text-secondary border border-dashed border-secondary rounded-xl bg-white hover:cursor-pointer hover:text-primary hover:border-solid">
-          <IoIosAdd className="inline mr-1 text-2xl" />
-          Add Subscription
-        </button>
+        <AddSubscriptionButton />
       </DrawerTrigger>
       <DrawerContent>
         <DrawerTitle className="p-5">
@@ -27,3 +54,19 @@ const AddSubscriptionDialog = () => {
 };
 
 export default AddSubscriptionDialog;
+
+const AddSubscriptionButton = forwardRef<
+  HTMLButtonElement,
+  ComponentProps<"button">
+>(({ ...props }, ref) => {
+  return (
+    <button
+      ref={ref}
+      className="btn flex items-center justify-center p-3 w-full font-semibold text-secondary border border-dashed border-secondary rounded-xl bg-white hover:cursor-pointer hover:text-primary hover:border-solid"
+      {...props}
+    >
+      <IoIosAdd className="inline mr-1 text-2xl" />
+      Add Subscription
+    </button>
+  );
+});
