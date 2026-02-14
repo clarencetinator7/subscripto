35337b53739b81f091831468d5247cb81a01748d
Clarencetinator <clarencejoseph.dimafelix@gmail.com>
2026-02-14 17:37:09 +0800

    feat: add EmptyState component and integrate into App for no subscriptions scenario

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import SubscriptionFormDialog from "./components/SubscriptionForm/SubscriptionFo
 import TreeMapRoot from "./components/TreeMap/TreeMapRoot";
 import { Button } from "./components/ui/button";
 import { useSubscriptionStore } from "./store/subscriptionStore";
+import EmptyState from "./components/EmptyState/EmptyState";
 
 // Github profile link: https://github.com/clarencetinator7
 
@@ -35,29 +36,33 @@ function App() {
             </Button>
           </div>
         </header>
+        {subscriptions.length === 0 && <EmptyState />}
         {subscriptions.length > 0 && <TreeMapRoot />}
         <SubscriptionFormDialog isEditMode={false} />
         <SubscriptionList />
       </div>
 
       {/* Floating Delete All Button */}
-      <div className="fixed bottom-6 left-1/2 -translate-x-1/2 w-full max-w-2xl flex justify-end pr-6 pointer-events-none">
-        <Button
-          variant="default"
-          onClick={() => {
-            if (
-              window.confirm(
-                "Are you sure you want to clear all subscriptions?",
-              )
-            ) {
-              clearAllSubscriptions();
-            }
-          }}
-          className="rounded-full px-7! py-6 flex items-center justify-center shadow-2xl bg-white text-gray-300  hover:shadow-xl hover:bg-white hover:text-red-500 hover:cursor-pointer pointer-events-auto"
-        >
-          <LuTrash2 className="text-xl" /> <span className="">Clear All </span>
-        </Button>
-      </div>
+      {subscriptions.length > 0 && (
+        <div className="fixed bottom-6 left-1/2 -translate-x-1/2 w-full max-w-2xl flex justify-end pr-6 pointer-events-none">
+          <Button
+            variant="default"
+            onClick={() => {
+              if (
+                window.confirm(
+                  "Are you sure you want to clear all subscriptions?",
+                )
+              ) {
+                clearAllSubscriptions();
+              }
+            }}
+            className="rounded-full px-7! py-6 flex items-center justify-center shadow-2xl bg-white text-gray-300  hover:shadow-xl hover:bg-white hover:text-red-500 hover:cursor-pointer pointer-events-auto"
+          >
+            <LuTrash2 className="text-xl" />{" "}
+            <span className="">Clear All </span>
+          </Button>
+        </div>
+      )}
     </>
   );
 }
